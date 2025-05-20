3e7ab48c0680edd10cf70d2a60954105b6c6a742
kemalergorun <kemalergorun@gmail.com>
2025-05-20 22:38:18 +0300

    200525

--- 01-basics/src/components/12-hooks/Hooks.jsx
+++ 01-basics/src/components/12-hooks/Hooks.jsx
@@ -5,19 +5,59 @@ import { useState } from "react";
 // HOOKS
 // 1- Hooks are used in function type components only, not in class type components.
 // 2- Hooks must be written on the top level, they cannot be used in conditional or loop blocks.
+// 3- Render (first load) X Re-render (every update after first load)
+
+// const [state, setState] = useState(initialState);
+// We can use Object, Array, or returned function in initial state.
+// If returned function is used in initial state, make sure that you use it without parenthesis.
 
 export default function Hooks() {
   //      getter setter
   const [number, setNumber] = useState(0);
+  const [name, setName] = useState("John Doe");
+  const [theme, setTheme] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+
+  const sum = (a, b) => {
+    console.log("I am rendered or re-rendered");
+    return a + b;
+  };
+
+  const [total, setTotal] = useState(sum);
 
   const handleClick = () => {
+    // We may encounter problems in frequent uses, if the new state is related with the previous state, as setNumber works asynchronously.
     setNumber((prev) => prev + 1);
     console.log(number);
   };
 
+  const handleClickName = () => {
+    setName("Mark Twain");
+  };
+
+  const handleTheme = () => {
+    setTheme({ ...theme, color: "white", backgroundColor: "black" });
+    console.log(theme);
+  };
+
+  const handleFunction = () => {
+    setTotal(sum(5, 7));
+  };
+
   return (
-    <Container>
+    <Container style={{ ...theme }}>
       <Button onClick={handleClick}>{number}</Button>
+      <Button variant="danger" onClick={handleClickName}>
+        {name}
+      </Button>
+      <Button variant="dark" onClick={handleTheme}>
+        Change Theme
+      </Button>
+      <Button variant="warning" onClick={handleFunction}>
+        func
+      </Button>
     </Container>
   );
 }
---
+++ 01-basics/src/components/13-counter-1/Counter1.jsx
@@ -0,0 +1,62 @@
+import Container from "react-bootstrap/Container";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+
+export default function Counter1() {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    // If current state is taking the reference of previous state all the time, then we should use callbacks to update state:
+    setCounter((previousValue) => previousValue + 1);
+  };
+
+  const handleDecrement = () => {
+    // If current state is taking the reference of previous state all the time, then we should use callbacks to update state:
+    setCounter((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    // If current state is NOT taking the reference of previous state, then we can give the value directly as follows:
+    setCounter(0);
+  };
+
+  return (
+    <Container>
+      <h2 className="text-center text-primary">Counter - 1</h2>
+
+      <ButtonGroup className="w-100 mt-5">
+        <Button
+          variant="danger"
+          type="button"
+          className="w-100"
+          onClick={handleIncrement}
+        >
+          +
+        </Button>
+
+        <Button variant="secondary" className="w-100" disabled>
+          {counter}
+        </Button>
+
+        <Button
+          variant="success"
+          className="w-100"
+          type="button"
+          onClick={handleDecrement}
+        >
+          -
+        </Button>
+
+        <Button
+          variant="info"
+          className="w-100"
+          type="button"
+          onClick={handleReset}
+        >
+          Reset
+        </Button>
+      </ButtonGroup>
+    </Container>
+  );
+}
